e741746474919ae65a32f25c70368ec455269e47
Madhur Rastogi <madhur.rastogi@primussoft.com>
2026-03-03 18:06:15 +0530

    MCU/DC

---
+++ DC/main.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "dc_bucket" {
+  bucket = "dc_is_better_than_marvel"
+}
---
+++ MCU/main.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "marvel-cinematic-universe" {
+  bucket = "mcu-202011121359"
+}
